6f692dcff0b8828ca2e483daf77f17e3773e6306
yvromero <yvonne.romero@uds.edu.py>
2024-07-31 11:46:15 -0400

    Configure upload files with secureURL

---
+++ src/files/files.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+    ) {
+
+      const path = this.filesService.getStaticProductImage( imageName );
+      
+      res.sendFile( path );
+    }
+
+  @Post('product')
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter,
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
+    })
+  }) )
+
+  uploadProductFile( 
+    @UploadedFile() file: Express.Multer.File,
+  ){
+
+
+    const secureUrl = `${ this.configService.get('HOST_API') }/files/product/${ file.filename }`;
+
+    return { secureUrl };
+  }
+}
